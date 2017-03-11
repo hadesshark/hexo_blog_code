f3b25f6654817bb3d0e892ef24e52ee941e880a9
hadesshark <hades_shark@hotmail.com>
2017-03-11 21:34:01 +0800

    code 修改好，且確定能用

--- source/downloads/code/shell/chapter1/ex1-1.sh
+++ source/downloads/code/shell/chapter1/ex1-1.sh
@@ -1,6 +1,7 @@
-for filename in 'ls .'
+# 顯示當下目錄的 json 
+for filename in `ls .`  # `ls .` 要用鍵整左上 esc 下的 `
 do
-  if echo "$filename" | grep "xml"
+  if echo "$filename" | grep "json"
   then
     echo "$filename"
   fi
